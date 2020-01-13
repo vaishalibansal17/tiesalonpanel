3f75088cc5be67a776918200f3b3b483a5cb6224
Abhishek Jaiswal <abhishek.jaiswal@brainmobi.com>
2020-01-13 19:13:14 +0530

    Full calender in process

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'; 
+// import { colors } from '../demo-utils/colors';
 import { HttpRequestService } from 'src/app/shared/service/http-request.service';
+import { EventInput } from '@fullcalendar/core';
+function getTimezoneOffsetString(date: Date): string {
+  const timezoneOffset = date.getTimezoneOffset();
+  const hoursOffset = String(
+    Math.floor(Math.abs(timezoneOffset / 60))
+  ).padStart(2, '0');
+  const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, '0');
+  const direction = timezoneOffset > 0 ? '-' : '+';
+  return `T00:00:00${direction}${hoursOffset}:${minutesOffset}`;
+}
+
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'mwl-demo-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  calendarPlugins =  [dayGridPlugin, timeGrigPlugin, interactionPlugin]; // important!
 
-  constructor(private http : HttpRequestService) { 
-    // this.http.getRequest('POST', 'ORGANISER_EVENT_LIST', { 'pageNo': 1, 'limitPage': 10, })
+  calendarEvents: EventInput[] = [
+    { title: 'Event Now', start: new Date() },
+    { title: 'Event Now', start: new Date() },{ title: 'Event Now', start: new Date() },{ title: 'Event Now', start: new Date() },
+    { title: 'Event Now', start: new Date() },{ title: 'Event Now', start: new Date() },{ title: 'Event Now', start: new Date() },
+    { title: 'Event Now', start: new Date() },{ title: 'Event Now', start: new Date() },{ title: 'Event Now', start: new Date() },
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+  }
+ 
+  handleDateClick(calDate) {
+    console.log(calDate);
   }
 
-  ngOnInit() {
+  eventClicked(calDate) {
+    console.log(calDate);
   }
-
 
 }
