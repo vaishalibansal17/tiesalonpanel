186b233f0d6614eb65e7d40b4d7f4523327150eb
Vaishali Bansal <vaishali.bansal@brainmobi.com>
2020-01-27 16:47:41 +0530

    Minor changes done

--- src/app/home/staff/staff.component.ts
+++ src/app/home/staff/staff.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  photo: number;
+  email: string;
+  number: number;
+  rating: number;
+  action: string;
+}
+
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
   styleUrls: ['./staff.component.scss']
 })
+
+
+
 export class StaffComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'photo', 'email', 'number', 'rating', 'action' ];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
@@ -13,3 +28,7 @@ export class StaffComponent implements OnInit {
   }
 
 }
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', photo: 1.0079, email: 'H', number: 1325, rating: 13214, action:'fdg' },
+];
