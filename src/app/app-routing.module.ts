557893ae52978ab73dba016fe5071678fa8c072d
Abhishek Jaiswal <abhishek.jaiswal@brainmobi.com>
2020-03-03 18:04:23 +0530

    PDF

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/guard/auth.guard';
 import { LoginGuard } from './shared/guard/login.guard';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), canActivate: [LoginGuard] },
   { path: 'reset-freelancer/:token', loadChildren: () => import('./reset-freelancer/reset-freelancer.module').then(m => m.ResetFreelancerModule) },
+  { path: 'invoice/:id', component: InvoiceComponent, canActivate: [AuthGuard]},
   { path: '**', component: PageNotFoundComponent }
 
 ];
---
+++ src/app/invoice/invoice.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpRequestService } from '../shared/service/http-request.service';
+import { ActivatedRoute } from '@angular/router';
+import { ErrorService } from '../shared/service/error.service';
+import * as jspdf from 'jspdf';
+import html2canvas from "html2canvas";
+import { DatePipe } from '@angular/common';
+(window as any).html2canvas = html2canvas;
+
+@Component({
+  selector: 'app-invoice',
+  templateUrl: './invoice.component.html',
+  styleUrls: ['./invoice.component.scss']
+})
+
+export class InvoiceComponent implements OnInit {
+  id: any;
+  detail: any;
+  ttlamt: any;
+  error: any;
+  @ViewChild('content', { static: true }) content: ElementRef;
+
+  constructor(private httpService: HttpRequestService, private routes: ActivatedRoute, private errsrv: ErrorService, ) { }
+
+  ngOnInit() {
+    this.id = this.routes.snapshot.params.id;
+    this.getUserProfile();
+  }
+
+  getUserProfile() {
+    this.httpService.getRequest('GET_PARMS', 'BOOKING_VIEW', this.id, '')
+      .subscribe((response: any) => {
+        if (response.status === 1) {
+          this.detail = response.res;
+          this.detail['slnme'] = JSON.parse(localStorage.getItem('salon')).name
+          this.ttlamt = this.detail.service.reduce((acc, val) => acc.cost + val.cost);
+          this.ttlamt = this.detail.service.length == 1 ? this.ttlamt.cost : this.ttlamt;
+        } else {
+          if (response.err) {
+            this.errsrv.handleError(response.err.errCode);
+          }
+        }
+      }, (error) => {
+        this.errsrv.handleError(0);
+        // this.httpService.showError(MESSAGE.CONNECTION_MSG, MESSAGE.CONNECTION_ERROR, MESSAGE.MSGTIME);
+      });
+  }
+
+
+  loadImage(url) {
+    return new Promise((resolve) => {
+      let img = new Image();
+      img.onload = () => resolve(img);
+      img.src = url;
+    })
+  }
+
+  htmlToPDF() {
+
+    // let doc = new jspdf();
+    // console.log('------------');
+    
+    // doc.addHTML(this.content.nativeElement, function() {
+    //   console.log('------------ppppppp');
+    //    doc.save("obrz.pdf");
+    // });
+    var element = this.content.nativeElement;
+    var HTML_Width = element.scrollWidth;
+    var HTML_Height = element.scrollHeight+100;
+    var top_left_margin = 15;
+    var PDF_Width = HTML_Width+(top_left_margin*2);
+    var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+    var canvas_image_width = HTML_Width;
+    var canvas_image_height = HTML_Height;
+    var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+     return html2canvas(element, {scale:2}).then(canvas => {
+       canvas.getContext('2d');
+       var imgData = canvas.toDataURL("image/jpeg", 1.0);
+       var pdf = new jspdf('p', 'pt',  [PDF_Width, PDF_Height]);
+      //  pdf.addImage(logo, 'JPEG',  400, 3, 400, 60);
+       pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin+40,canvas_image_width,canvas_image_height);
+
+       for (var i = 1; i <= totalPDFPages; i++) {
+         pdf.addPage(PDF_Width, PDF_Height);
+         pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+       }
+       let datePipe = new DatePipe("en-US");
+        pdf.save(name+' '+datePipe.transform(new Date(), 'MMM-dd-yyyy')+'.pdf'); // Generated PDF
+       // this.toastr.info("Please be patient we're processing the pdf", "PDF Processing!");
+       return true;
+     })
+
+  }
+
+
+}
